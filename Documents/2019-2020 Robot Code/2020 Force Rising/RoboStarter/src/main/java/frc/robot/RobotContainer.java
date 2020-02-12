2bdbdaf564db4bcdd375dd8cd0d05dec74d3be38
Tim Holt <robotics@timholt.org>
2020-02-12 15:48:25 -0700

    New commands and subsystems added

--- Documents/2019-2020 Robot Code/2020 Force Rising/RoboStarter/src/main/java/frc/robot/RobotContainer.java
+++ Documents/2019-2020 Robot Code/2020 Force Rising/RoboStarter/src/main/java/frc/robot/RobotContainer.java
@@ -11,9 +11,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.DriveCommand;
-import frc.robot.subsystems.DriveTrain;
-import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.*;
+//import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.*;
 //import frc.robot.commands.ExampleCommand;
 //import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -29,10 +29,10 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final DriveTrain m_driveTrain = new DriveTrain();
   private final XboxController m_xboxController = new XboxController(Constants.DRIVER_CONTROLLER);
+  private final DriveTrain m_driveTrain = new DriveTrain();
+  private final Command m_autoCommand = new AutoCommand();
   private final DriveCommand m_driveCommand = new DriveCommand(m_driveTrain, m_xboxController);
-
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -61,6 +61,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_driveCommand;
+    return m_autoCommand;
   }
 }
